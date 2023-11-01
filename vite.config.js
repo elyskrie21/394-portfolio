a938b31a837081b6eb64baf742e5f9dc59b35cb4
Elyse Kriegel <esamkriegel@gmail.com>
2023-10-31 19:28:17 -0500

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 /** @type {import('vite').UserConfig} */
+import { resolve } from 'path'
 export default {
   //absolute path
   base: '/394-portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'pages/contact.html'),
+        essay: resolve(__dirname, 'pages/essay.html'),
+        essay: resolve(__dirname, 'pages/works.html'),
+      },
+    },
+  }
 };
